e76dee20940cdf1a77d54cd51221235fab460bf6
HOFFMAN <19hoffmani@gmail.com>
2018-09-10 09:14:11 -0500

    notes

---
+++ arrays.js
@@ -0,0 +1,26 @@
+movies = ["Lilo and Stitch", "Hawaii 5-0", "Jurassic Park"];
+/*console.log(movies[1]);
+console.log(movies[0]);
+console.log(movies[2]);
+console.log(movies.length)
+console.log(movies.log)
+const elements = ["Hello", 7, { message: "Hi mom" }, true];
+//************ITERATING OVER AN ARRAY************\\
+for (i = 0; i < movies.length; i++){
+    console.log(movies[i])
+}
+//using the for-of method
+let ofMovie;
+for (ofMovie of movies){
+    console.log(ofMovie);
+}
+/*movies.forEach(myElement =>{
+    console.log(`For Loop ${myElement}`);
+});*/
+//ADDING TO AN ARRAY 
+movies.push("Magnum PI");
+
+for (i = 0; i < movies.length; i++){
+    console.log(movies[i])
+}
+console.log(movies[movies.length - 1]);
